108fd74f727109c041687242091a5abde2a1ab18
lotosbin <lotosbin@gmail.com>
2023-07-10 11:25:40 +0800

    build: migrate to androidx

--- app/src/main/java/com/wanjian/demo/ExceptionView.java
+++ app/src/main/java/com/wanjian/demo/ExceptionView.java
@@ -2,7 +2,7 @@ package com.wanjian.demo;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/wanjian/demo/LifecycleExceptionActivity.java
+++ app/src/main/java/com/wanjian/demo/LifecycleExceptionActivity.java
@@ -3,7 +3,7 @@ package com.wanjian.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
